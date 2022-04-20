5600a8dafe41241503b899e8b20896c0d141f28a
lukeskywalkerZ <93051844+lukeskywalkerZ@users.noreply.github.com>
2022-04-20 11:52:43 -0300

    Create script.js

---
+++ colecoes/script.js
@@ -0,0 +1,17 @@
+function getAdmins(map) {
+    let admins = [];
+
+    for([key, value] of map) {
+        if(value === 'Admin') {
+            admins.push(key)
+        }
+    }
+    return admins;
+}
+
+const usuarios = new Map();
+
+usuarios.set('Lucas', 'Admin')
+usuarios.set('Tadeu', 'Admin')
+usuarios.set('Marcos', 'User')
+usuarios.set('Ednilson', 'Admin')
